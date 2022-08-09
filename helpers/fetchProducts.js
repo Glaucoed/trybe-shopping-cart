a4e9f9e334eaf98cdce8d7def3db6a2ee3974e30
Glaucoed <gesverzuthe@gmail.com>
2022-08-09 17:34:51 -0300

    erro lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,12 +1,13 @@
 const fetchProducts = async (item) => {
   const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
   const response = await fetch(API_URL);
-  const product = await response.json();
+  const products = await response.json();
+  // console.log(products)
   
   if (!item) {
     throw new Error('You must provide an url');
   }
-  return product;
+  return products;
 };
 if (typeof module !== 'undefined') {
   module.exports = {
