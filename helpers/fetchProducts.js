57f5b9c8a1f29787beb1d724f0acb99557bab747
Glaucoed <gesverzuthe@gmail.com>
2022-08-09 17:05:48 -0300

    realizado requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,13 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+  const response = await fetch(API_URL);
+  const product = await response.json();
+  
+  if (!item) {
+    throw new Error('You must provide an url');
+  }
+  return product;
 };
-
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,22 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toEqual('function');
+  });
+  it('Verifique a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async() => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Verifique se ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint', async() => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  });
+  it('Verifique se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const retornoFunction = await fetchProducts('computador')
+    expect(retornoFunction).toEqual(computadorSearch)
+  });
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url"', async() => {
+  await expect(fetchProducts()).rejects.toThrow('You must provide an url');
+  });
 });
